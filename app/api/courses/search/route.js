ba921e9dfbfefda35acbfdff58575f3624e4de26
fadeomar <fadeomar2015@gmail.com>
2023-09-23 23:52:51 +0300

    static search params in search

--- app/api/courses/search/route.js
+++ app/api/courses/search/route.js
@@ -1,9 +1,10 @@
-import { NextResponse } from 'next/server';
-import courses from '../data.json';
+import { NextResponse } from "next/server";
+import courses from "../data.json";
 
 export async function GET(request) {
-  const { searchParams } = new URL(request.url);
-  const query = searchParams.get('query');
+  // const { searchParams } = new URL(request.url);
+  const searchParams = "test";
+  const query = searchParams.get("query");
   const filteredCourses = courses.filter((course) => {
     return course.title.toLowerCase().includes(query.toLowerCase());
   });
